c4cecfcf47f2075a542dc66e4bfdb5e3af371a7c
Lara Bautista <lara.bautista16@gmail.com>
2021-06-17 19:59:01 +0200

    create empty dart project

---
+++ lib/views/MyHomePage.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+/// This is the stateless widget that the main application instantiates.
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DataTable(
+      columns: const <DataColumn>[
+        DataColumn(
+          label: Text(
+            'Name',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
+        ),
+        DataColumn(
+          label: Text(
+            'Age',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
+        ),
+        DataColumn(
+          label: Text(
+            'Designation',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
+        ),
+      ],
+      rows: const <DataRow>[
+        DataRow(
+          cells: <DataCell>[
+            DataCell(Text('Mohit')),
+            DataCell(Text('23')),
+            DataCell(Text('Associate Software Developer')),
+          ],
+        ),
+        DataRow(
+          cells: <DataCell>[
+            DataCell(Text('Akshay')),
+            DataCell(Text('25')),
+            DataCell(Text('Software Developer')),
+          ],
+        ),
+        DataRow(
+          cells: <DataCell>[
+            DataCell(Text('Deepak')),
+            DataCell(Text('29')),
+            DataCell(Text('Team Lead ')),
+          ],
+        ),
+      ],
+    );
+  }
+}
